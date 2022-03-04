fef2865d57fd9d9f3dede244f20d504e55eff113
Royrocks <rlhudson@ucdavis.edu>
2022-03-04 15:19:01 -0800

    added secret.py

--- Titanic/Titanic/settings.py
+++ Titanic/Titanic/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w))52+l(!+qhe2*ga#s(tm+w#%v-uv*(l+kz5js+7+(5_4(dp0'
+from . import secret
+SECRET_KEY = secret.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
